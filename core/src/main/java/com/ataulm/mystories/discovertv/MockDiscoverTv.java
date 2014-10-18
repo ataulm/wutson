b3138134ccd3b71e05663e553e038d7c892242f7
Ataul Munim <timberwo7ves@gmail.com>
2014-10-18 21:39:12 +0100

    Add mock JSON response for DiscoverTV api call

---
+++ core/src/main/java/com/ataulm/mystories/discovertv/MockDiscoverTv.java
@@ -0,0 +1,292 @@
+package com.ataulm.mystories.discovertv;
+
+public class MockDiscoverTv {
+
+    public static final String JSON = "{\n" +
+            "    \"page\": 1,\n" +
+            "    \"results\": [\n" +
+            "        {\n" +
+            "            \"backdrop_path\": null,\n" +
+            "            \"id\": 61508,\n" +
+            "            \"original_name\": \"Benched\",\n" +
+            "            \"first_air_date\": \"2014-10-15\",\n" +
+            "            \"origin_country\": [\n" +
+            "                \"US\"\n" +
+            "            ],\n" +
+            "            \"poster_path\": \"/beR0BZsIt1NrjOCd5PZbXrpvb2.jpg\",\n" +
+            "            \"popularity\": 2.8125,\n" +
+            "            \"name\": \"Benched\",\n" +
+            "            \"vote_average\": 0,\n" +
+            "            \"vote_count\": 0\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"backdrop_path\": \"/y3iZibhyEmFCrdRENGaSZHM2fSm.jpg\",\n" +
+            "            \"id\": 1399,\n" +
+            "            \"original_name\": \"Game of Thrones\",\n" +
+            "            \"first_air_date\": \"2011-04-17\",\n" +
+            "            \"origin_country\": [\n" +
+            "                \"US\"\n" +
+            "            ],\n" +
+            "            \"poster_path\": \"/pVC9o9LLnhuPpas0B0p9spqAxNg.jpg\",\n" +
+            "            \"popularity\": 1.875,\n" +
+            "            \"name\": \"Game of Thrones\",\n" +
+            "            \"vote_average\": 9.4,\n" +
+            "            \"vote_count\": 135\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"backdrop_path\": \"/ipPpVwGS0VdtnxF8iEjrD8oryeA.jpg\",\n" +
+            "            \"id\": 1621,\n" +
+            "            \"original_name\": \"Boardwalk Empire\",\n" +
+            "            \"first_air_date\": \"2010-09-19\",\n" +
+            "            \"origin_country\": [\n" +
+            "                \"US\"\n" +
+            "            ],\n" +
+            "            \"poster_path\": \"/yXZ0ooun7zNNBS9kuHS3TYlnYZ4.jpg\",\n" +
+            "            \"popularity\": 1.875,\n" +
+            "            \"name\": \"Boardwalk Empire\",\n" +
+            "            \"vote_average\": 8.3,\n" +
+            "            \"vote_count\": 11\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"backdrop_path\": \"/144sKAEfXdM2VNrZzHuMSUr9zAa.jpg\",\n" +
+            "            \"id\": 1401,\n" +
+            "            \"original_name\": \"Hell on Wheels\",\n" +
+            "            \"first_air_date\": \"2011-11-06\",\n" +
+            "            \"origin_country\": [\n" +
+            "                \"US\"\n" +
+            "            ],\n" +
+            "            \"poster_path\": \"/rfWcvfiCaqrWWCs1uAGL9Inz2Cy.jpg\",\n" +
+            "            \"popularity\": 1.875,\n" +
+            "            \"name\": \"Hell on Wheels\",\n" +
+            "            \"vote_average\": 8.6,\n" +
+            "            \"vote_count\": 7\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"backdrop_path\": \"/6rWBXDT04nbJCutsDbpuCwbrOtw.jpg\",\n" +
+            "            \"id\": 1405,\n" +
+            "            \"original_name\": \"Dexter\",\n" +
+            "            \"first_air_date\": \"2006-10-01\",\n" +
+            "            \"origin_country\": [\n" +
+            "                \"US\"\n" +
+            "            ],\n" +
+            "            \"poster_path\": \"/eIoAzf9czDSWqYneE1fZTQ3iJhB.jpg\",\n" +
+            "            \"popularity\": 1.875,\n" +
+            "            \"name\": \"Dexter\",\n" +
+            "            \"vote_average\": 8.8,\n" +
+            "            \"vote_count\": 52\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"backdrop_path\": \"/yIRifynhrjlqC93S86E2j8VeS6k.jpg\",\n" +
+            "            \"id\": 1416,\n" +
+            "            \"original_name\": \"Grey's Anatomy\",\n" +
+            "            \"first_air_date\": \"2005-03-27\",\n" +
+            "            \"origin_country\": [\n" +
+            "                \"US\"\n" +
+            "            ],\n" +
+            "            \"poster_path\": \"/6dycBL60Q2zZNuFLC95wu2ijzFF.jpg\",\n" +
+            "            \"popularity\": 1.875,\n" +
+            "            \"name\": \"Grey's Anatomy\",\n" +
+            "            \"vote_average\": 8.2,\n" +
+            "            \"vote_count\": 12\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"backdrop_path\": \"/nGsNruW3W27V6r4gkyc3iiEGsKR.jpg\",\n" +
+            "            \"id\": 1418,\n" +
+            "            \"original_name\": \"The Big Bang Theory\",\n" +
+            "            \"first_air_date\": \"2007-09-24\",\n" +
+            "            \"origin_country\": [\n" +
+            "                \"US\"\n" +
+            "            ],\n" +
+            "            \"poster_path\": \"/mYOWdfjuYqiZC1VS2SPefW3g38Q.jpg\",\n" +
+            "            \"popularity\": 1.875,\n" +
+            "            \"name\": \"The Big Bang Theory\",\n" +
+            "            \"vote_average\": 8.5,\n" +
+            "            \"vote_count\": 81\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"backdrop_path\": \"/nALNtBMQVf27H4HiSZnKNfFVvXX.jpg\",\n" +
+            "            \"id\": 2734,\n" +
+            "            \"original_name\": \"Law & Order: Special Victims Unit\",\n" +
+            "            \"first_air_date\": \"1999-09-20\",\n" +
+            "            \"origin_country\": [\n" +
+            "                \"US\"\n" +
+            "            ],\n" +
+            "            \"poster_path\": \"/tsxiLPFlpNlJaMFlKIFqffGkiJt.jpg\",\n" +
+            "            \"popularity\": 1.875,\n" +
+            "            \"name\": \"Law & Order: Special Victims Unit\",\n" +
+            "            \"vote_average\": 8.5,\n" +
+            "            \"vote_count\": 6\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"backdrop_path\": \"/6HswtHbletDHrR8B84U5KWkXheQ.jpg\",\n" +
+            "            \"id\": 502,\n" +
+            "            \"original_name\": \"Sesame Street\",\n" +
+            "            \"first_air_date\": \"1969-11-10\",\n" +
+            "            \"origin_country\": [\n" +
+            "                \"US\"\n" +
+            "            ],\n" +
+            "            \"poster_path\": \"/aM2VEnR05ssgmafDGNFngJWyOrC.jpg\",\n" +
+            "            \"popularity\": 1.875,\n" +
+            "            \"name\": \"Sesame Street\",\n" +
+            "            \"vote_average\": 8.3,\n" +
+            "            \"vote_count\": 2\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"backdrop_path\": \"/xXOEbDe1ChzPHLOCfFoLyFDxQ2A.jpg\",\n" +
+            "            \"id\": 6467,\n" +
+            "            \"original_name\": \"Chicago Hope\",\n" +
+            "            \"first_air_date\": \"1994-09-28\",\n" +
+            "            \"origin_country\": [\n" +
+            "                \"US\"\n" +
+            "            ],\n" +
+            "            \"poster_path\": \"/xyWHvpa73jspstjIefBvGz53Nde.jpg\",\n" +
+            "            \"popularity\": 1.875,\n" +
+            "            \"name\": \"Chicago Hope\",\n" +
+            "            \"vote_average\": 0,\n" +
+            "            \"vote_count\": 0\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"backdrop_path\": \"/soUz8M9MTHJeS6oK8i3VDpKraJx.jpg\",\n" +
+            "            \"id\": 46896,\n" +
+            "            \"original_name\": \"The Originals\",\n" +
+            "            \"first_air_date\": \"2013-10-03\",\n" +
+            "            \"origin_country\": [\n" +
+            "                \"US\"\n" +
+            "            ],\n" +
+            "            \"poster_path\": \"/7RNQ8N6ZD3G3NF7g9RnwQdwdkan.jpg\",\n" +
+            "            \"popularity\": 1.875,\n" +
+            "            \"name\": \"The Originals\",\n" +
+            "            \"vote_average\": 8.8,\n" +
+            "            \"vote_count\": 4\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"backdrop_path\": \"/u2l8ujrNxE1uV9Pt2pMLTI6YEg2.jpg\",\n" +
+            "            \"id\": 61463,\n" +
+            "            \"original_name\": \"The Affair\",\n" +
+            "            \"first_air_date\": \"2014-10-12\",\n" +
+            "            \"origin_country\": [\n" +
+            "                \"US\"\n" +
+            "            ],\n" +
+            "            \"poster_path\": \"/1XP3SNIGwqcE8KPF68D5vyPTBtw.jpg\",\n" +
+            "            \"popularity\": 1.875,\n" +
+            "            \"name\": \"The Affair\",\n" +
+            "            \"vote_average\": 10,\n" +
+            "            \"vote_count\": 1\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"backdrop_path\": null,\n" +
+            "            \"id\": 61490,\n" +
+            "            \"original_name\": \"rahaei\",\n" +
+            "            \"first_air_date\": \"2014-10-13\",\n" +
+            "            \"origin_country\": [],\n" +
+            "            \"poster_path\": null,\n" +
+            "            \"popularity\": 1.875,\n" +
+            "            \"name\": \"rahaei\",\n" +
+            "            \"vote_average\": 0,\n" +
+            "            \"vote_count\": 0\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"backdrop_path\": \"/1L8JCGfpIp5BK5yiwrddW7ztLAL.jpg\",\n" +
+            "            \"id\": 1918,\n" +
+            "            \"original_name\": \"The Twilight Zone\",\n" +
+            "            \"first_air_date\": \"1985-09-27\",\n" +
+            "            \"origin_country\": [\n" +
+            "                \"US\"\n" +
+            "            ],\n" +
+            "            \"poster_path\": \"/gPbXTcnyisKxHi4Owkzi1IEjoHg.jpg\",\n" +
+            "            \"popularity\": 1.875,\n" +
+            "            \"name\": \"The Twilight Zone\",\n" +
+            "            \"vote_average\": 0,\n" +
+            "            \"vote_count\": 0\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"backdrop_path\": \"/aX9dKOVnquJRBFWmhIgbBXb6Fxv.jpg\",\n" +
+            "            \"id\": 456,\n" +
+            "            \"original_name\": \"The Simpsons\",\n" +
+            "            \"first_air_date\": \"1989-12-17\",\n" +
+            "            \"origin_country\": [\n" +
+            "                \"US\"\n" +
+            "            ],\n" +
+            "            \"poster_path\": \"/oadXgs77zoLQm04J9dG4fQ3Huwn.jpg\",\n" +
+            "            \"popularity\": 1.875,\n" +
+            "            \"name\": \"The Simpsons\",\n" +
+            "            \"vote_average\": 8.8,\n" +
+            "            \"vote_count\": 49\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"backdrop_path\": \"/scivLGg7zdqmdw5eDW3rvrEavr6.jpg\",\n" +
+            "            \"id\": 1412,\n" +
+            "            \"original_name\": \"Arrow\",\n" +
+            "            \"first_air_date\": \"2012-10-10\",\n" +
+            "            \"origin_country\": [\n" +
+            "                \"US\",\n" +
+            "                \"CA\"\n" +
+            "            ],\n" +
+            "            \"poster_path\": \"/bJWEC62XVGLWw7PjJy6oOwxbBHF.jpg\",\n" +
+            "            \"popularity\": 1.875,\n" +
+            "            \"name\": \"Arrow\",\n" +
+            "            \"vote_average\": 8.5,\n" +
+            "            \"vote_count\": 38\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"backdrop_path\": \"/pY0SZqPZ1uDUUDoeEwWeMxPrv2J.jpg\",\n" +
+            "            \"id\": 1215,\n" +
+            "            \"original_name\": \"Californication\",\n" +
+            "            \"first_air_date\": \"2007-08-13\",\n" +
+            "            \"origin_country\": [\n" +
+            "                \"US\"\n" +
+            "            ],\n" +
+            "            \"poster_path\": \"/qynpyBArLSmg4wJVoGDzBA82x1M.jpg\",\n" +
+            "            \"popularity\": 1.875,\n" +
+            "            \"name\": \"Californication\",\n" +
+            "            \"vote_average\": 8.8,\n" +
+            "            \"vote_count\": 17\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"backdrop_path\": \"/bzoZjhbpriBT2N5kwgK0weUfVOX.jpg\",\n" +
+            "            \"id\": 1396,\n" +
+            "            \"original_name\": \"Breaking Bad\",\n" +
+            "            \"first_air_date\": \"2008-01-19\",\n" +
+            "            \"origin_country\": [\n" +
+            "                \"US\"\n" +
+            "            ],\n" +
+            "            \"poster_path\": \"/4yMXf3DW6oCL0lVPZaZM2GypgwE.jpg\",\n" +
+            "            \"popularity\": 1.875,\n" +
+            "            \"name\": \"Breaking Bad\",\n" +
+            "            \"vote_average\": 9.1,\n" +
+            "            \"vote_count\": 128\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"backdrop_path\": \"/4liFvkke5RDaNdEMPdIBtGtgTLT.jpg\",\n" +
+            "            \"id\": 1100,\n" +
+            "            \"original_name\": \"How I Met Your Mother\",\n" +
+            "            \"first_air_date\": \"2005-09-19\",\n" +
+            "            \"origin_country\": [\n" +
+            "                \"US\"\n" +
+            "            ],\n" +
+            "            \"poster_path\": \"/mFmqUdXlwbiKbsXZk1yb2hAYHqV.jpg\",\n" +
+            "            \"popularity\": 1.875,\n" +
+            "            \"name\": \"How I Met Your Mother\",\n" +
+            "            \"vote_average\": 8.1,\n" +
+            "            \"vote_count\": 39\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"backdrop_path\": \"/3yLECa8OqWys9yl7vE53apjoDsO.jpg\",\n" +
+            "            \"id\": 4385,\n" +
+            "            \"original_name\": \"The Colbert Report\",\n" +
+            "            \"first_air_date\": \"2005-10-17\",\n" +
+            "            \"origin_country\": [\n" +
+            "                \"US\"\n" +
+            "            ],\n" +
+            "            \"poster_path\": \"/7mwErPneNN2BUAODW2gldnhL8Oe.jpg\",\n" +
+            "            \"popularity\": 1.875,\n" +
+            "            \"name\": \"The Colbert Report\",\n" +
+            "            \"vote_average\": 8,\n" +
+            "            \"vote_count\": 2\n" +
+            "        }\n" +
+            "    ],\n" +
+            "    \"total_pages\": 3050,\n" +
+            "    \"total_results\": 60985\n" +
+            "}\n";
+
+}
