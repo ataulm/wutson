e9b4a616a6a7b2bd80616458f33614cef4ea9663
Ataul Munim <timberwo7ves@gmail.com>
2014-10-23 23:16:44 +0100

    Add javadoc for Configuration showing how to build img url

--- core/src/main/java/com/ataulm/mystories/configuration/Configuration.java
+++ core/src/main/java/com/ataulm/mystories/configuration/Configuration.java
@@ -2,6 +2,20 @@ package com.ataulm.mystories.configuration;
 
 import java.util.List;
 
+/**
+ * Get the system wide configuration information.
+ *
+ * Some elements of the API require some knowledge of this configuration data.
+ * The purpose of this is to try and keep the actual API responses as light as possible.
+ * It is recommended you cache this data within your application and check for updates every few days.
+ *
+ * This method currently holds the data relevant to building image URLs as well as the change key map.
+ *
+ * To build an image URL, you will need 3 pieces of data. The base_url, size and file_path.
+ * Simply combine them all and you will have a fully qualified URL.
+ *
+ * Here’s an example URL: http://image.tmdb.org/t/p/w500/8uO0gUM8aNqYLs1OsTBQiXu0fEv.jpg
+ */
 public class Configuration {
 
     private final static Configuration EMPTY = new Configuration(null);
