212e3f9365c5595517767a92586f870fc2a0ac29
Ataul Munim <timberwo7ves@gmail.com>
2014-10-26 20:13:46 +0000

    Extract TrendingShows adapter from Activity

--- android/src/main/java/com/ataulm/mystories/TrendingShowsActivity.java
+++ android/src/main/java/com/ataulm/mystories/TrendingShowsActivity.java
@@ -4,10 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.view.ViewGroup;
 
-import java.util.Collections;
 import java.util.List;
 
 public class TrendingShowsActivity extends Activity {
@@ -17,65 +14,16 @@ public class TrendingShowsActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trending_shows);
 
-        setupTrendingTvShowsList();
-    }
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.trending_shows_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        TrendingShowsAdapter adapter = TrendingShowsAdapter.newInstance();
+        recyclerView.setAdapter(adapter);
 
-    private void setupTrendingTvShowsList() {
-        RecyclerView trendingShowsList = (RecyclerView) findViewById(R.id.trending_shows_list);
-        TrendingShowsAdapter trendingShowsAdapter = TrendingShowsAdapter.newInstance();
-        trendingShowsList.setAdapter(trendingShowsAdapter);
-        trendingShowsList.setLayoutManager(new LinearLayoutManager(this));
-
-        List<Show> trendingShows = getTrendingShows();
-        trendingShowsAdapter.updateWith(trendingShows);
+        adapter.onUpdate(getTrendingShows());
     }
 
     private List<Show> getTrendingShows() {
-        ShowsProvider showsProvider = new ShowsProvider();
-        return showsProvider.fetchTrendingShows();
-    }
-
-    private static class TrendingShowsAdapter extends RecyclerView.Adapter<TrendingShowsViewHolder> {
-
-        private List<Show> shows;
-
-        static TrendingShowsAdapter newInstance() {
-            return new TrendingShowsAdapter(Collections.<Show>emptyList());
-        }
-
-        TrendingShowsAdapter(List<Show> shows) {
-            this.shows = shows;
-        }
-
-        public void updateWith(List<Show> shows) {
-            this.shows = shows;
-            notifyDataSetChanged();
-        }
-
-        @Override
-        public TrendingShowsViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-            // TODO: create view holder
-            return null;
-        }
-
-        @Override
-        public void onBindViewHolder(TrendingShowsViewHolder viewHolder, int i) {
-            // TODO: bind data to view?
-        }
-
-        @Override
-        public int getItemCount() {
-            return shows.size();
-        }
-
-    }
-
-    private static class TrendingShowsViewHolder extends RecyclerView.ViewHolder  {
-
-        public TrendingShowsViewHolder(View itemView) {
-            super(itemView);
-        }
-
+        return new ShowsProvider().fetchTrendingShows();
     }
 
 }
---
+++ android/src/main/java/com/ataulm/mystories/TrendingShowsAdapter.java
@@ -0,0 +1,63 @@
+package com.ataulm.mystories;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import java.util.Collections;
+import java.util.List;
+
+class TrendingShowsAdapter extends RecyclerView.Adapter<TrendingShowsViewHolder> {
+
+    private static final int VIEW_TYPE_STANDARD = 1;
+    private static final int VIEW_TYPE_EMPHASISED = 2;
+
+    private List<Show> shows;
+
+    static TrendingShowsAdapter newInstance() {
+        return new TrendingShowsAdapter(Collections.<Show>emptyList());
+    }
+
+    TrendingShowsAdapter(List<Show> shows) {
+        this.shows = shows;
+    }
+
+    public void onUpdate(List<Show> shows) {
+        this.shows = shows;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public TrendingShowsViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(viewGroup.getContext());
+        TrendingShowsItemView view;
+        switch (viewType) {
+            case VIEW_TYPE_STANDARD:
+                view = (TrendingShowsItemView) layoutInflater.inflate(R.layout.view_trending_shows_list_standard, viewGroup, false);
+                break;
+            case VIEW_TYPE_EMPHASISED:
+                view = (TrendingShowsItemView) layoutInflater.inflate(R.layout.view_trending_shows_list_emphasised, viewGroup, false);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot create ViewHolder for unknown viewType: " + viewType);
+        }
+        return new TrendingShowsViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(TrendingShowsViewHolder viewHolder, int position) {
+        Show show = shows.get(position);
+        viewHolder.bindViewTo(show);
+    }
+
+    @Override
+    public int getItemCount() {
+        return shows.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return super.getItemViewType(position);
+    }
+
+}
