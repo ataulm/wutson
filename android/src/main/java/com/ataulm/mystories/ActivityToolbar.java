cd9fb475476802545aa90fbdf6cfde52ee2db784
Ataul Munim <timberwo7ves@gmail.com>
2014-11-02 10:43:40 +0000

    Add blank activity toolbar

---
+++ android/src/main/java/com/ataulm/mystories/ActivityToolbar.java
@@ -0,0 +1,24 @@
+package com.ataulm.mystories;
+
+import android.content.Context;
+import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+
+public class ActivityToolbar extends Toolbar {
+
+    public ActivityToolbar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ActivityToolbar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int desiredHeight = getResources().getDimensionPixelSize(R.dimen.actionbar_height);
+        int desiredHeightMeasureSpec = MeasureSpec.makeMeasureSpec(desiredHeight, MeasureSpec.EXACTLY);
+        super.onMeasure(widthMeasureSpec, desiredHeightMeasureSpec);
+    }
+
+}
