c9fd73f46f3e3401a5666aaecc3b9aab96d7b436
Ataul Munim <timberwo7ves@gmail.com>
2014-11-30 11:16:44 +0000

    Add show title to trending shows item

--- android/src/main/java/com/ataulm/wutson/TrendingShowsItemView.java
+++ android/src/main/java/com/ataulm/wutson/TrendingShowsItemView.java
@@ -4,8 +4,11 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 public class TrendingShowsItemView extends FrameLayout {
+
+    private TextView titleView;
 
     public TrendingShowsItemView(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
@@ -13,13 +16,17 @@ public class TrendingShowsItemView extends FrameLayout {
 
     public TrendingShowsItemView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        // TODO: dis fudged up UX. Y U NO LISTEN? - Romain Guy, probably
-        setFocusableInTouchMode(true);
     }
 
     @Override
     protected void onFinishInflate() {
         View.inflate(getContext(), R.layout.merge_trending_shows_list_standard, this);
+
+        titleView = (TextView) findViewById(R.id.trending_shows_item_title);
+    }
+
+    public void update(Show show) {
+        titleView.setText(show.getName());
     }
 
 }
--- android/src/main/java/com/ataulm/wutson/TrendingShowsViewHolder.java
+++ android/src/main/java/com/ataulm/wutson/TrendingShowsViewHolder.java
@@ -1,7 +1,5 @@
 package com.ataulm.wutson;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Toast;
@@ -9,21 +7,28 @@ import android.widget.Toast;
 class TrendingShowsViewHolder extends RecyclerView.ViewHolder {
 
     private final TrendingShowsItemView trendingShowsItemView;
+    private static Toast toast;
 
     public TrendingShowsViewHolder(TrendingShowsItemView itemView) {
         super(itemView);
         this.trendingShowsItemView = itemView;
+        // TODO: instead, use this as a legacy viewholder, not holderview
     }
 
     public void present(final Show show) {
         trendingShowsItemView.setContentDescription(show.getName());
+        trendingShowsItemView.update(show);
         trendingShowsItemView.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                Toast.makeText(v.getContext(), show.getName(), Toast.LENGTH_SHORT).show();
-                Activity activity = (Activity) v.getContext();
-                activity.startActivity(new Intent(activity, ShowActivity.class));
+                if (toast != null) {
+                    toast.cancel();
+                }
+                toast = Toast.makeText(v.getContext(), show.getName(), Toast.LENGTH_SHORT);
+                toast.show();
+//                Activity activity = (Activity) v.getContext();
+//                activity.startActivity(new Intent(activity, ShowActivity.class));
             }
 
         });
