de64b5f31a2ba0f07e9a3a142f7c3127b3c92b9a
Ataul Munim <timberwo7ves@gmail.com>
2014-12-14 14:25:49 +0000

    Update dependencies for tests

---
+++ core/src/test/java/com/ataulm/wutson/tmdb/configuration/DummyTest.java
@@ -0,0 +1,16 @@
+package com.ataulm.wutson.tmdb.configuration;
+
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class DummyTest {
+
+    @Test
+    public void dummyTestToShowUnitTestingWorks() {
+        boolean expected = true;
+        boolean actual = true;
+        assertThat(actual).isEqualTo(expected);
+    }
+
+}
