53ae50def9b6d27c4f7987926c233c4b040a32cb
Ataul Munim <timberwo7ves@gmail.com>
2014-12-25 11:02:52 +0000

    Add doc for Provider interface

--- core/src/main/java/com/ataulm/wutson/Provider.java
+++ core/src/main/java/com/ataulm/wutson/Provider.java
@@ -2,6 +2,11 @@ package com.ataulm.wutson;
 
 public interface Provider<T> {
 
+    /**
+     * Provides an instance of T.
+     * <p/>
+     * This may be a long operation - it's performed on the same thread that it's called on.
+     */
     T provide();
 
 }
