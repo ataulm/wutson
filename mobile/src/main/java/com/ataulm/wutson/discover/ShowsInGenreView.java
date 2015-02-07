581346613149410d473c0a4bf5210a54bfa6fb05
Ataul Munim <timberwo7ves@gmail.com>
2015-02-07 23:11:35 +0000

    Add grid for discover list items

--- mobile/src/main/java/com/ataulm/wutson/discover/ShowsInGenreView.java
+++ mobile/src/main/java/com/ataulm/wutson/discover/ShowsInGenreView.java
@@ -1,7 +1,7 @@
 package com.ataulm.wutson.discover;
 
 import android.content.Context;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
@@ -13,6 +13,8 @@ import android.widget.TextView;
 import com.ataulm.wutson.R;
 
 public class ShowsInGenreView extends FrameLayout {
+
+    private static final int SPAN_COUNT = 2;
 
     private Adapter adapter;
 
@@ -28,7 +30,7 @@ public class ShowsInGenreView extends FrameLayout {
     protected void onFinishInflate() {
         View.inflate(getContext(), R.layout.merge_shows_in_genre, this);
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.shows_in_genre_list);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), SPAN_COUNT));
 
         adapter = new Adapter(LayoutInflater.from(getContext()));
         recyclerView.setAdapter(adapter);
@@ -54,7 +56,7 @@ public class ShowsInGenreView extends FrameLayout {
 
         @Override
         public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = layoutInflater.inflate(R.layout.view_shows_in_genre_list_item, parent, false);
+            View view = layoutInflater.inflate(R.layout.item_shows_in_genre, parent, false);
             return new ViewHolder(view);
         }
 
