a716539b2698b1b4e0154b6b0edd2ff83f28f186
Ataul Munim <timberwo7ves@gmail.com>
2015-02-14 21:29:37 +0000

    Apply body text color instead of title, for greater contrast

--- mobile/src/main/java/com/ataulm/wutson/discover/ShowsInGenreItemView.java
+++ mobile/src/main/java/com/ataulm/wutson/discover/ShowsInGenreItemView.java
@@ -143,7 +143,7 @@ public class ShowsInGenreItemView extends FrameLayout implements Displayer<Show>
         if (posterShouldStillDisplay(show)) {
             Palette.Swatch swatch = getAvailableSwatch(palette);
             nameTextView.setBackgroundColor(swatch.getRgb());
-            nameTextView.setTextColor(swatch.getTitleTextColor());
+            nameTextView.setTextColor(swatch.getBodyTextColor());
         }
     }
 
