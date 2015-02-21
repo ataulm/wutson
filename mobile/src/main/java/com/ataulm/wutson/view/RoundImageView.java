7443be17785c9a28bdb42d5e8e05a9644d65f000
Ataul Munim <timberwo7ves@gmail.com>
2015-02-21 11:31:47 +0000

    Add RoundImageView for cast in ShowDetails

---
+++ mobile/src/main/java/com/ataulm/wutson/view/RoundImageView.java
@@ -0,0 +1,12 @@
+package com.ataulm.wutson.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class RoundImageView extends de.hdodenhof.circleimageview.CircleImageView {
+
+    public RoundImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
