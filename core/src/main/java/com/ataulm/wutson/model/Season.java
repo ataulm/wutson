b2c0ff5f0e64e7249e7ddd479419e9e96146e2ea
Ataul Munim <timberwo7ves@gmail.com>
2015-02-22 14:48:09 +0000

    Add season attributes to parser

--- core/src/main/java/com/ataulm/wutson/model/Season.java
+++ core/src/main/java/com/ataulm/wutson/model/Season.java
@@ -1,4 +1,19 @@
 package com.ataulm.wutson.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Season {
+
+    @SerializedName("id")
+    String id;
+
+    @SerializedName("episode_count")
+    int size;
+
+    @SerializedName("poster_path")
+    String posterPath;
+
+    @SerializedName("season_number")
+    int seasonNumber;
+
 }
