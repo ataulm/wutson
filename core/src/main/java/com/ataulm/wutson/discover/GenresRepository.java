05e04cea25d938df921946cf6eeb45ebf760c3cc
Ataul Munim <timberwo7ves@gmail.com>
2015-03-08 09:45:24 +0000

    Update Genres behaviourSubject to use `hasValue`

--- core/src/main/java/com/ataulm/wutson/discover/GenresRepository.java
+++ core/src/main/java/com/ataulm/wutson/discover/GenresRepository.java
@@ -1,10 +1,9 @@
 package com.ataulm.wutson.discover;
 
-import com.ataulm.wutson.tmdb.gson.GsonGenres;
 import com.ataulm.wutson.tmdb.TmdbApi;
+import com.ataulm.wutson.tmdb.gson.GsonGenres;
 
 import rx.Observable;
-import rx.functions.Action1;
 import rx.schedulers.Schedulers;
 import rx.subjects.BehaviorSubject;
 
@@ -13,15 +12,13 @@ public class GenresRepository {
     private final TmdbApi api;
     private final BehaviorSubject<GsonGenres> subject;
 
-    private boolean initialised;
-
     GenresRepository(TmdbApi api) {
         this.api = api;
         this.subject = BehaviorSubject.create();
     }
 
     Observable<GsonGenres> getGenres() {
-        if (!initialised) {
+        if (!subject.hasValue()) {
             refreshGenres();
         }
         return subject;
@@ -29,20 +26,8 @@ public class GenresRepository {
 
     private void refreshGenres() {
         api.getGenres()
-                .doOnNext(markAsInitialised())
                 .subscribeOn(Schedulers.io())
                 .subscribe(subject);
-    }
-
-    private Action1<GsonGenres> markAsInitialised() {
-        return new Action1<GsonGenres>() {
-
-            @Override
-            public void call(GsonGenres gsonGenres) {
-                initialised = true;
-            }
-
-        };
     }
 
 }
