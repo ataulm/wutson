e369d54c6630bd820762ae1cfdcccaf0c8fb8e03
Ataul Munim <timberwo7ves@gmail.com>
2015-03-15 20:51:12 +0000

    Remove CastView

--- mobile/src/main/java/com/ataulm/wutson/show/ShowOverviewView.java
+++ mobile/src/main/java/com/ataulm/wutson/show/ShowOverviewView.java
@@ -2,10 +2,14 @@ package com.ataulm.wutson.show;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.Resources;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.ataulm.wutson.R;
@@ -13,16 +17,23 @@ import com.bumptech.glide.Glide;
 
 import java.net.URI;
 
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+
 // not a problem - https://code.google.com/p/android/issues/detail?id=67434
 @SuppressLint("Instantiatable")
 class ShowOverviewView extends FrameLayout {
 
+    private final LayoutInflater layoutInflater;
+
     private ImageView backdropImageView;
     private TextView showHeaderView;
-    private CastView castView;
+    private View castView;
+    private ViewGroup castMembersContainer;
 
     public ShowOverviewView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        this.layoutInflater = LayoutInflater.from(context);
     }
 
     @Override
@@ -30,7 +41,8 @@ class ShowOverviewView extends FrameLayout {
         View.inflate(getContext(), R.layout.merge_show_overview, this);
         backdropImageView = (ImageView) findViewById(R.id.show_view_image_backdrop);
         showHeaderView = (TextView) findViewById(R.id.show_header_text_overview);
-        castView = (CastView) findViewById(R.id.show_view_cast);
+        castView = findViewById(R.id.show_view_cast);
+        castMembersContainer = (ViewGroup) findViewById(R.id.cast_container);
     }
 
     void setBackdrop(URI backdropUri) {
@@ -44,7 +56,33 @@ class ShowOverviewView extends FrameLayout {
     }
 
     void setCast(Cast cast) {
-        castView.display(cast);
+        castMembersContainer.removeAllViews();
+
+        if (cast.size() == 0) {
+            castView.setVisibility(GONE);
+            return;
+        }
+
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT);
+        Resources resources = getResources();
+        layoutParams.setMargins(
+                resources.getDimensionPixelSize(R.dimen.character_margin_left),
+                resources.getDimensionPixelSize(R.dimen.character_margin_top),
+                resources.getDimensionPixelSize(R.dimen.character_margin_right),
+                resources.getDimensionPixelSize(R.dimen.character_margin_bottom));
+
+        castView.setVisibility(VISIBLE);
+
+        for (Character character : cast) {
+            View characterView = getViewFor(character);
+            castMembersContainer.addView(characterView, layoutParams);
+        }
+    }
+
+    private View getViewFor(Character character) {
+        CharacterView characterView = ((CharacterView) layoutInflater.inflate(R.layout.view_character, this, false));
+        characterView.display(character);
+        return characterView;
     }
 
 }
