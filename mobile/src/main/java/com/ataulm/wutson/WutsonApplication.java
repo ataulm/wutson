bd808d9c71b29fd3064f41b85b5be0f17dbf32f9
Ataul Munim <timberwo7ves@gmail.com>
2015-03-17 08:43:04 +0000

    Add Crashlytics grade setup

--- mobile/src/main/java/com/ataulm/wutson/WutsonApplication.java
+++ mobile/src/main/java/com/ataulm/wutson/WutsonApplication.java
@@ -1,12 +1,14 @@
 package com.ataulm.wutson;
 
 import android.app.Application;
+import com.crashlytics.android.Crashlytics;
 
 public class WutsonApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Crashlytics.start(this);
         Jabber.init(this);
     }
 
