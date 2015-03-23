57eaa46f5af2943b1a4ed7dd188d81689e27fa9c
Ataul Munim <timberwo7ves@gmail.com>
2015-03-23 19:50:54 +0000

    Add persistence for tracked shows

--- mobile/src/main/java/com/ataulm/wutson/showdetails/ShowDetailsActivity.java
+++ mobile/src/main/java/com/ataulm/wutson/showdetails/ShowDetailsActivity.java
@@ -1,10 +1,13 @@
 package com.ataulm.wutson.showdetails;
 
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.astuetz.PagerSlidingTabStrip;
 import com.ataulm.wutson.BuildConfig;
@@ -15,6 +18,7 @@ import com.ataulm.wutson.rx.LoggingObserver;
 
 import java.net.URI;
 
+import rx.Observer;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
@@ -24,8 +28,10 @@ public class ShowDetailsActivity extends WutsonActivity implements OnClickSeason
     public static final String EXTRA_SHOW_TITLE = BuildConfig.APPLICATION_ID + ".show_title";
     public static final String EXTRA_SHOW_BACKDROP = BuildConfig.APPLICATION_ID + ".show_backdrop";
 
+    private Subscription trackedStatusSubscription;
     private Subscription showDetailsSubscription;
     private ShowPagerAdapter adapter;
+    private TrackedShowsRepository trackedShowsRepository;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,6 +41,11 @@ public class ShowDetailsActivity extends WutsonActivity implements OnClickSeason
         ViewPager viewPager = (ViewPager) findViewById(R.id.show_details_pager_show);
         viewPager.setAdapter(adapter = new ShowPagerAdapter(getResources(), this, getLayoutInflater(), getShowBackdropUri()));
         ((PagerSlidingTabStrip) findViewById(R.id.show_details_tabs_show)).setViewPager(viewPager);
+
+
+        SharedPreferences sharedPreferences = getSharedPreferences("TRACKED_SHOWS_REPO", MODE_PRIVATE);
+        // TODO: move to DataRepository
+        trackedShowsRepository = TrackedShowsRepository.newInstance(sharedPreferences);
     }
 
     private URI getShowBackdropUri() {
@@ -69,12 +80,27 @@ public class ShowDetailsActivity extends WutsonActivity implements OnClickSeason
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.show_details, menu);
+
+        MenuItem trackMenuItem = menu.findItem(R.id.show_details_menu_item_toggle_track);
+        Observer<Boolean> observer = new TrackingShowObserver(trackMenuItem);
+
+        trackedStatusSubscription = trackedShowsRepository.getTrackedStatusOfShowWith(getShowId())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(observer);
+
+        return true;
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
         showDetailsSubscription = Jabber.dataRepository().getShow(getShowId())
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer());
+                .subscribe(new ShowObserver());
     }
 
     private String getShowId() {
@@ -86,6 +112,9 @@ public class ShowDetailsActivity extends WutsonActivity implements OnClickSeason
         if (!showDetailsSubscription.isUnsubscribed()) {
             showDetailsSubscription.unsubscribe();
         }
+        if (!trackedStatusSubscription.isUnsubscribed()) {
+            trackedStatusSubscription.unsubscribe();
+        }
         super.onPause();
     }
 
@@ -94,7 +123,39 @@ public class ShowDetailsActivity extends WutsonActivity implements OnClickSeason
         navigate().toSeason(getShowId(), season.getSeasonNumber());
     }
 
-    private class Observer extends LoggingObserver<Show> {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.show_details_menu_item_toggle_track) {
+            trackedShowsRepository.toggleTrackingShowWithId(getShowId());
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private class TrackingShowObserver extends LoggingObserver<Boolean> {
+
+        private final MenuItem item;
+
+        private TrackingShowObserver(MenuItem item) {
+            this.item = item;
+        }
+
+        @Override
+        public void onNext(Boolean trackingShow) {
+            super.onNext(trackingShow);
+            if (trackingShow) {
+                item.setIcon(R.drawable.abc_btn_rating_star_on_mtrl_alpha);
+                item.setTitle("Stop tracking show");
+            } else {
+                item.setIcon(R.drawable.abc_btn_rating_star_off_mtrl_alpha);
+                item.setTitle("Start tracking show");
+            }
+        }
+
+    }
+
+    private class ShowObserver extends LoggingObserver<Show> {
 
         @Override
         public void onNext(Show show) {
---
+++ mobile/src/main/java/com/ataulm/wutson/showdetails/TrackedShowsRepository.java
@@ -0,0 +1,63 @@
+package com.ataulm.wutson.showdetails;
+
+import android.content.SharedPreferences;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import rx.Observable;
+import rx.subjects.BehaviorSubject;
+
+final class TrackedShowsRepository {
+
+    private static final String KEY_TRACKED_SHOWS = "key_tracked_shows";
+
+    private final SharedPreferences sharedPreferences;
+    private final Set<String> idsOfTrackedShows;
+    private final BehaviorSubject<Boolean> subject;
+
+    static TrackedShowsRepository newInstance(SharedPreferences sharedPreferences) {
+        Set<String> idsOfTrackedShows = sharedPreferences.getStringSet(KEY_TRACKED_SHOWS, new HashSet<String>());
+        return new TrackedShowsRepository(idsOfTrackedShows, sharedPreferences);
+    }
+
+    private TrackedShowsRepository(Set<String> idsOfTrackedShows, SharedPreferences sharedPreferences) {
+        this.sharedPreferences = sharedPreferences;
+        this.subject = BehaviorSubject.create();
+        this.idsOfTrackedShows = idsOfTrackedShows;
+    }
+
+    Observable<Boolean> getTrackedStatusOfShowWith(String showId) {
+        if (!subject.hasValue()) {
+            subject.onNext(idsOfTrackedShows.contains(showId));
+        }
+        return subject;
+    }
+
+    void toggleTrackingShowWithId(String showId) {
+        if (idsOfTrackedShows.contains(showId)) {
+            stopTrackingShowWithId(showId);
+        } else {
+            startTrackingShowWithId(showId);
+        }
+    }
+
+    private void startTrackingShowWithId(String showId) {
+        if (idsOfTrackedShows.add(showId)) {
+            resynchronisePrefs();
+        }
+        subject.onNext(true);
+    }
+
+    private void stopTrackingShowWithId(String showId) {
+        if (idsOfTrackedShows.remove(showId)) {
+            resynchronisePrefs();
+        }
+        subject.onNext(false);
+    }
+
+    private void resynchronisePrefs() {
+        sharedPreferences.edit().putStringSet(KEY_TRACKED_SHOWS, idsOfTrackedShows).apply();
+    }
+
+}
