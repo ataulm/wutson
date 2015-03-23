c5d082710c11ca43640d712635e337ca7ac5e2e4
Ataul Munim <timberwo7ves@gmail.com>
2015-03-23 13:54:54 +0000

    Swap initial launcher activity to MyShows

--- mobile/src/main/java/com/ataulm/wutson/myshows/MyShowsActivity.java
+++ mobile/src/main/java/com/ataulm/wutson/myshows/MyShowsActivity.java
@@ -1,11 +1,14 @@
 package com.ataulm.wutson.myshows;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.ataulm.wutson.R;
 import com.ataulm.wutson.navigation.NavigationDrawerItem;
 import com.ataulm.wutson.navigation.WutsonTopLevelActivity;
+
+import java.util.Set;
 
 public class MyShowsActivity extends WutsonTopLevelActivity {
 
@@ -14,9 +17,20 @@ public class MyShowsActivity extends WutsonTopLevelActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (activityWasOpenedFromLauncher()) {
+            Log.d("TVSHOW", "is launcher");
+        } else {
+            Log.d("TVSHOW", "is not launcher");
+        }
+
         setContentView(R.layout.activity_my_shows);
 
         myShowsTextView = (TextView) findViewById(R.id.test);
+    }
+
+    private boolean activityWasOpenedFromLauncher() {
+        Set<String> categories = getIntent().getCategories();
+        return categories != null && categories.contains("android.intent.category.LAUNCHER");
     }
 
     @Override
