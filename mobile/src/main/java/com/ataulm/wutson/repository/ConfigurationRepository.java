dd9004246ff6752a4d548288d4eb2f85e4bce8fc
Ataul Munim <timberwo7ves@gmail.com>
2015-03-28 22:29:37 +0000

    Remove new Gson() in configuration refresh

--- mobile/src/main/java/com/ataulm/wutson/repository/ConfigurationRepository.java
+++ mobile/src/main/java/com/ataulm/wutson/repository/ConfigurationRepository.java
@@ -9,6 +9,7 @@ import com.google.gson.Gson;
 import rx.Observable;
 import rx.Subscriber;
 import rx.functions.Action1;
+import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 import rx.subjects.BehaviorSubject;
 
@@ -17,11 +18,14 @@ public class ConfigurationRepository {
     private final TmdbApi api;
     private final PersistentDataRepository persistentDataRepository;
     private final BehaviorSubject<GsonConfiguration> subject;
+    private final Gson gson;
 
     ConfigurationRepository(TmdbApi api, PersistentDataRepository persistentDataRepository) {
         this.api = api;
         this.persistentDataRepository = persistentDataRepository;
         this.subject = BehaviorSubject.create();
+
+        this.gson = new Gson();
     }
 
     public Observable<GsonConfiguration> getConfiguration() {
@@ -32,37 +36,54 @@ public class ConfigurationRepository {
     }
 
     private void refreshConfiguration() {
-        fetchGsonConfigurationFrom(persistentDataRepository)
-                .switchIfEmpty(api.getConfiguration().doOnNext(saveTo(persistentDataRepository)))
+        fetchJsonConfigurationFrom(persistentDataRepository)
+                .flatMap(asGsonConfiguration(gson))
+                .switchIfEmpty(api.getConfiguration().doOnNext(saveTo(persistentDataRepository, gson)))
                 .lift(Function.<GsonConfiguration>swallowOnCompleteEvents())
                 .subscribeOn(Schedulers.io())
                 .subscribe(subject);
     }
 
-    private static Observable<GsonConfiguration> fetchGsonConfigurationFrom(final PersistentDataRepository repository) {
-        return Observable.create(new Observable.OnSubscribe<GsonConfiguration>() {
+    private static Observable<String> fetchJsonConfigurationFrom(final PersistentDataRepository repository) {
+        return Observable.create(new Observable.OnSubscribe<String>() {
 
             @Override
-            public void call(Subscriber<? super GsonConfiguration> subscriber) {
-                String json = repository.readJsonConfiguration();
-                if (!json.isEmpty()) {
-                    // TODO: this new Gson() doesn't belong here
-                    GsonConfiguration gsonConfiguration = new Gson().fromJson(json, GsonConfiguration.class);
-                    subscriber.onNext(gsonConfiguration);
-                }
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext(repository.readJsonConfiguration());
                 subscriber.onCompleted();
             }
 
         });
     }
 
-    private static Action1<GsonConfiguration> saveTo(final PersistentDataRepository persistentDataRepository) {
+    private static Func1<String, Observable<GsonConfiguration>> asGsonConfiguration(final Gson gson) {
+        return new Func1<String, Observable<GsonConfiguration>>() {
+
+            @Override
+            public Observable<GsonConfiguration> call(final String json) {
+                return Observable.create(new Observable.OnSubscribe<GsonConfiguration>() {
+
+                    @Override
+                    public void call(Subscriber<? super GsonConfiguration> subscriber) {
+                        if (!json.isEmpty()) {
+                            GsonConfiguration gsonConfiguration = gson.fromJson(json, GsonConfiguration.class);
+                            subscriber.onNext(gsonConfiguration);
+                        }
+                        subscriber.onCompleted();
+                    }
+
+                });
+            }
+
+        };
+    }
+
+    private static Action1<GsonConfiguration> saveTo(final PersistentDataRepository persistentDataRepository, final Gson gson) {
         return new Action1<GsonConfiguration>() {
 
             @Override
             public void call(GsonConfiguration gsonConfiguration) {
-                // TODO: this new Gson() doesn't belong here
-                String json = new Gson().toJson(gsonConfiguration, GsonConfiguration.class);
+                String json = gson.toJson(gsonConfiguration, GsonConfiguration.class);
                 persistentDataRepository.writeJsonConfiguration(json);
             }
 
