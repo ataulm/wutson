52991b228a681037c60c48a2d057d3ca6e28efa3
Ataul Munim <timberwo7ves@gmail.com>
2015-03-30 07:18:59 +0100

    Add TmdbConfiguration model

---
+++ mobile/src/main/java/com/ataulm/wutson/model/TmdbConfiguration.java
@@ -0,0 +1,43 @@
+package com.ataulm.wutson.model;
+
+import java.net.URI;
+import java.util.List;
+
+public class TmdbConfiguration {
+
+    private static final int PROFILE_SIZE_STANDARD = 1;
+    private static final int POSTER_SIZE_STANDARD = 2;
+    private static final int BACKDROP_SIZE_STANDARD = 2;
+    private static final int STILL_SIZE_STANDARD = 2;
+
+    private final String baseUrl;
+    private final List<String> profileImageSizes;
+    private final List<String> posterImageSizes;
+    private final List<String> backdropImageSizes;
+    private final List<String> stillImageSizes;
+
+    TmdbConfiguration(String baseUrl, List<String> profileImageSizes, List<String> posterImageSizes, List<String> backdropImageSizes, List<String> stillImageSizes) {
+        this.baseUrl = baseUrl;
+        this.profileImageSizes = profileImageSizes;
+        this.posterImageSizes = posterImageSizes;
+        this.backdropImageSizes = backdropImageSizes;
+        this.stillImageSizes = stillImageSizes;
+    }
+
+    URI completeStill(String partialPath) {
+        return URI.create(baseUrl + stillImageSizes.get(STILL_SIZE_STANDARD) + partialPath);
+    }
+
+    URI completePoster(String partialPath) {
+        return URI.create(baseUrl + posterImageSizes.get(POSTER_SIZE_STANDARD) + partialPath);
+    }
+
+    URI completeBackdrop(String partialPath) {
+        return URI.create(baseUrl + backdropImageSizes.get(BACKDROP_SIZE_STANDARD) + partialPath);
+    }
+
+    URI completeProfile(String partialPath) {
+        return URI.create(baseUrl + profileImageSizes.get(PROFILE_SIZE_STANDARD) + partialPath);
+    }
+
+}
