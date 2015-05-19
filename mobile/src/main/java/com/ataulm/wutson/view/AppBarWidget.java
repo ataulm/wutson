1911e4d424fbeb73a9a7937adf5cc856bbb9365f
Ataul Munim <timberwo7ves@gmail.com>
2015-05-19 22:41:17 +0100

    Update AppBarWidget to be a container of Toolbar and other shiz
    - adds background color as themeable element
    - custom layout as styleable element

---
+++ mobile/src/main/java/com/ataulm/wutson/view/AppBarWidget.java
@@ -0,0 +1,52 @@
+package com.ataulm.wutson.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.ataulm.wutson.DeveloperError;
+import com.ataulm.wutson.R;
+
+public class AppBarWidget extends LinearLayout {
+
+    private Toolbar toolbar;
+
+    public AppBarWidget(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        applyCustomAttributes(context, attrs);
+    }
+
+    private void applyCustomAttributes(Context context, AttributeSet attrs) {
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.AppBarWidget);
+        int mergeLayoutId = typedArray.getResourceId(R.styleable.AppBarWidget_mergeLayoutId, R.layout.merge_app_bar_default);
+        int backgroundColor = typedArray.getResourceId(R.styleable.AppBarWidget_appBarBackgroundColor, getResources().getColor(R.color.transparent));
+        typedArray.recycle();
+
+        setBackgroundResource(backgroundColor);
+        View.inflate(getContext(), mergeLayoutId, this);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        super.setOrientation(VERTICAL);
+
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        if (toolbar == null) {
+            throw DeveloperError.because("Custom app bar layouts must contain a Toolbar with id='@id/app_bar'");
+        }
+    }
+
+    @Override
+    public final void setOrientation(int orientation) {
+        throw DeveloperError.because("This widget has fixed orientation.");
+    }
+
+    public Toolbar getToolbar() {
+        return toolbar;
+    }
+
+}
