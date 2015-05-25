cd840c2e535789387efa106657f5aca935437b39
Ataul Munim <timberwo7ves@gmail.com>
2015-05-25 20:28:31 +0100

    Rename tab strip app bar layout to be more generic

--- mobile/src/main/java/com/ataulm/wutson/showdetails/ShowDetailsActivity.java
+++ mobile/src/main/java/com/ataulm/wutson/showdetails/ShowDetailsActivity.java
@@ -18,7 +18,6 @@ import com.ataulm.wutson.rx.LoggingObserver;
 
 import java.net.URI;
 
-import rx.Observer;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
@@ -41,7 +40,7 @@ public class ShowDetailsActivity extends WutsonActivity implements OnClickSeason
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.show_details_pager_show);
         viewPager.setAdapter(adapter = new ShowPagerAdapter(getResources(), this, getLayoutInflater(), getShowBackdropUri()));
-        ((PagerSlidingTabStrip) findViewById(R.id.show_details_tabs_show)).setViewPager(viewPager);
+        ((PagerSlidingTabStrip) findViewById(R.id.tab_strip)).setViewPager(viewPager);
     }
 
     private URI getShowBackdropUri() {
