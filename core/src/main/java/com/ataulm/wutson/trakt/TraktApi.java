a5df3245325f430fe85279308d6ef4c062980bc3
Ataul Munim <timberwo7ves@gmail.com>
2015-07-19 19:44:29 +0100

    Increase Discover query limit to 100 items per page

--- core/src/main/java/com/ataulm/wutson/trakt/TraktApi.java
+++ core/src/main/java/com/ataulm/wutson/trakt/TraktApi.java
@@ -6,10 +6,10 @@ import rx.Observable;
 
 public interface TraktApi {
 
-    @GET("/shows/trending?extended=images")
+    @GET("/shows/trending?extended=images&limit=100")
     Observable<GsonTrendingShowList> getTrendingShows();
 
-    @GET("/shows/popular?extended=images")
+    @GET("/shows/popular?extended=images&limit=100")
     Observable<GsonPopularShowList> getPopularShows();
 
     @GET("/shows/{id}?extended=images,full")
