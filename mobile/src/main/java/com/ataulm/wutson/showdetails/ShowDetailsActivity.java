2b8f92fc212758ee345cd5073f086b08f07cc96d
Ataul Munim <timberwo7ves@gmail.com>
2015-08-09 19:25:17 +0100

    Remove Track stuff from ShowDetails

--- mobile/src/main/java/com/ataulm/wutson/showdetails/ShowDetailsActivity.java
+++ mobile/src/main/java/com/ataulm/wutson/showdetails/ShowDetailsActivity.java
@@ -6,8 +6,6 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.ColorInt;
 import android.support.v4.view.ViewPager;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.ataulm.wutson.BuildConfig;
 import com.ataulm.wutson.R;
@@ -16,7 +14,6 @@ import com.ataulm.wutson.navigation.WutsonActivity;
 import com.ataulm.wutson.rx.LoggingObserver;
 import com.ataulm.wutson.shows.Show;
 import com.ataulm.wutson.shows.ShowId;
-import com.ataulm.wutson.shows.TrackedStatus;
 import com.novoda.landingstrip.LandingStrip;
 
 import java.net.URI;
@@ -33,7 +30,6 @@ public class ShowDetailsActivity extends WutsonActivity implements OnClickSeason
     public static final String EXTRA_SHOW_BACKDROP = BuildConfig.APPLICATION_ID + ".show_backdrop";
     public static final String EXTRA_SHOW_ACCENT_COLOR = BuildConfig.APPLICATION_ID + ".show_accent_color";
 
-    private Subscription trackedStatusSubscription;
     private Subscription showDetailsSubscription;
     private ShowDetailsPagerAdapter adapter;
 
@@ -48,11 +44,11 @@ public class ShowDetailsActivity extends WutsonActivity implements OnClickSeason
     }
 
     private URI getShowBackdropUri() {
-        Bundle extras = getIntent().getExtras();
-        if (extras == null) {
-            return URI.create("");
-        }
-        return URI.create(extras.getString(EXTRA_SHOW_BACKDROP, ""));
+        return URI.create(getExtras().getString(EXTRA_SHOW_BACKDROP, ""));
+    }
+
+    private Bundle getExtras() {
+        return getIntent().getExtras() != null ? getIntent().getExtras() : Bundle.EMPTY;
     }
 
     @Override
@@ -68,18 +64,8 @@ public class ShowDetailsActivity extends WutsonActivity implements OnClickSeason
                 .subscribe(new ShowObserver());
     }
 
-    @ColorInt
-    private int getAccentColor() {
-        Bundle extras = getIntent().getExtras();
-        return extras.getInt(EXTRA_SHOW_ACCENT_COLOR, getResources().getColor(R.color.show_details_app_bar_background));
-    }
-
     private void applyTitleFromIntentExtras() {
-        Bundle extras = getIntent().getExtras();
-        if (extras == null) {
-            return;
-        }
-        String showTitle = extras.getString(EXTRA_SHOW_TITLE, "");
+        String showTitle = getExtras().getString(EXTRA_SHOW_TITLE, getString(R.string.show_details_label));
         setTitle(showTitle);
     }
 
@@ -90,17 +76,10 @@ public class ShowDetailsActivity extends WutsonActivity implements OnClickSeason
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_show_details, menu);
-
-        MenuItem trackMenuItem = menu.findItem(R.id.show_details_menu_item_toggle_track);
-        trackedStatusSubscription = dataRepository().getTrackedStatus(getShowId())
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new TrackingShowObserver(trackMenuItem));
-
-        return true;
+    @ColorInt
+    private int getAccentColor() {
+        int fallbackColor = getResources().getColor(R.color.show_details_app_bar_background);
+        return getExtras().getInt(EXTRA_SHOW_ACCENT_COLOR, fallbackColor);
     }
 
     private ShowId getShowId() {
@@ -112,56 +91,12 @@ public class ShowDetailsActivity extends WutsonActivity implements OnClickSeason
         if (!showDetailsSubscription.isUnsubscribed()) {
             showDetailsSubscription.unsubscribe();
         }
-        if (!trackedStatusSubscription.isUnsubscribed()) {
-            trackedStatusSubscription.unsubscribe();
-        }
         super.onDestroy();
     }
 
     @Override
     public void onClick(Show.SeasonSummary seasonSummary) {
         navigate().toSeason(getShowId(), seasonSummary.getSeasonNumber());
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.show_details_menu_item_toggle_track) {
-            dataRepository().toggleTrackedStatus(getShowId());
-            return true;
-        } else {
-            return super.onOptionsItemSelected(item);
-        }
-    }
-
-    private class TrackingShowObserver extends LoggingObserver<TrackedStatus> {
-
-        private final MenuItem item;
-
-        TrackingShowObserver(MenuItem item) {
-            super(Jabber.log());
-            this.item = item;
-        }
-
-        @Override
-        public void onNext(TrackedStatus trackedStatus) {
-            super.onNext(trackedStatus);
-            if (trackedStatus == TrackedStatus.TRACKED) {
-                updateMenuItemToReflectThatShowIsBeingTracked();
-            } else {
-                updateMenuItemToReflectThatShowIsNotBeingTracked();
-            }
-        }
-
-        private void updateMenuItemToReflectThatShowIsBeingTracked() {
-            item.setIcon(R.drawable.ic_action_star_full);
-            item.setTitle("Stop tracking show");
-        }
-
-        private void updateMenuItemToReflectThatShowIsNotBeingTracked() {
-            item.setIcon(R.drawable.ic_action_star);
-            item.setTitle("Start tracking show");
-        }
-
     }
 
     private class ShowObserver extends LoggingObserver<Show> {
