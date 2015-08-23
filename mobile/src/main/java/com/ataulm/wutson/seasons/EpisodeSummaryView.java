83dd756f5ca08ba9a394d792380078ac74334f91
Ataul Munim <timberwo7ves@gmail.com>
2015-08-23 11:58:43 +0100

    Add season/episode number to EpisodeSummary

--- mobile/src/main/java/com/ataulm/wutson/seasons/EpisodeSummaryView.java
+++ mobile/src/main/java/com/ataulm/wutson/seasons/EpisodeSummaryView.java
@@ -5,18 +5,19 @@ import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.ataulm.wutson.R;
 import com.ataulm.wutson.episodes.Episode;
+import com.ataulm.wutson.episodes.SeasonEpisodeNumber;
 import com.bumptech.glide.Glide;
 
 public class EpisodeSummaryView extends CardView {
 
     private ImageView posterImageView;
-    private TextView episodeNameTextView;
     private TextView episodeNumberTextView;
+    private TextView episodeNameTextView;
+    private TextView episodeAirDateTextView;
 
     public EpisodeSummaryView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -27,13 +28,44 @@ public class EpisodeSummaryView extends CardView {
         super.onFinishInflate();
         View.inflate(getContext(), R.layout.merge_episode_summary, this);
         posterImageView = (ImageView) findViewById(R.id.episode_summary_image_poster);
+        episodeNumberTextView = (TextView) findViewById(R.id.episode_summary_text_episode_number);
         episodeNameTextView = (TextView) findViewById(R.id.episode_summary_text_episode_name);
-        episodeNumberTextView = (TextView) findViewById(R.id.episode_summary_text_episode_air_date);
+        episodeAirDateTextView = (TextView) findViewById(R.id.episode_summary_text_episode_air_date);
     }
 
     void display(Episode episode) {
         updateStillPoster(episode);
+        updateEpisodeNumber(episode);
         updateAirDate(episode);
+        updateEpisodeName(episode);
+    }
+
+    private void updateEpisodeNumber(Episode episode) {
+        SeasonEpisodeNumber seasonEpisodeNumber = episode.getSeasonEpisodeNumber();
+        if (seasonEpisodeNumber.getSeason() == 0) {
+            episodeNumberTextView.setVisibility(GONE);
+            episodeNameTextView.setPadding(
+                    episodeNameTextView.getPaddingLeft(),
+                    getResources().getDimensionPixelSize(R.dimen.episode_summary_name_padding_top_if_episode_number_not_present),
+                    episodeNameTextView.getPaddingRight(),
+                    episodeNameTextView.getPaddingBottom()
+            );
+        } else {
+            episodeNumberTextView.setVisibility(VISIBLE);
+            episodeNameTextView.setPadding(
+                    episodeNameTextView.getPaddingLeft(),
+                    getResources().getDimensionPixelSize(R.dimen.episode_summary_name_margin_top),
+                    episodeNameTextView.getPaddingRight(),
+                    episodeNameTextView.getPaddingBottom()
+            );
+        }
+
+        String episodeNumber = getResources().getString(R.string.episode_summary_season_number_episode_number_format, seasonEpisodeNumber.getSeason(), seasonEpisodeNumber.getEpisode());
+        episodeNumberTextView.setText(episodeNumber);
+    }
+
+    private void updateEpisodeName(Episode episode) {
+        episodeNameTextView.setText(episode.getName());
     }
 
     private void updateStillPoster(Episode episode) {
@@ -42,15 +74,14 @@ public class EpisodeSummaryView extends CardView {
                 .centerCrop()
                 .error(R.drawable.ic_season_or_episode_placeholder)
                 .into(posterImageView);
-        episodeNameTextView.setText(episode.getName());
     }
 
     private void updateAirDate(Episode episode) {
         if (episode.getAirDate().isValid()) {
-            episodeNumberTextView.setText(episode.getAirDate().toString());
-            episodeNumberTextView.setVisibility(VISIBLE);
+            episodeAirDateTextView.setText(episode.getAirDate().toString());
+            episodeAirDateTextView.setVisibility(VISIBLE);
         } else {
-            episodeNumberTextView.setVisibility(GONE);
+            episodeAirDateTextView.setVisibility(GONE);
         }
     }
 
