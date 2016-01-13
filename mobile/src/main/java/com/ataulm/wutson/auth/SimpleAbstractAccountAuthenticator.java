a93ea917564873b42f9e23f711a73d1100107c62
Ataul Munim <hello@ataulm.com>
2016-01-13 18:48:24 +0000

    Remove unused imports on SimpleAccountAuthenticator

--- mobile/src/main/java/com/ataulm/wutson/auth/SimpleAbstractAccountAuthenticator.java
+++ mobile/src/main/java/com/ataulm/wutson/auth/SimpleAbstractAccountAuthenticator.java
@@ -3,30 +3,9 @@ package com.ataulm.wutson.auth;
 import android.accounts.AbstractAccountAuthenticator;
 import android.accounts.Account;
 import android.accounts.AccountAuthenticatorResponse;
-import android.accounts.AccountManager;
 import android.accounts.NetworkErrorException;
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-
-import com.ataulm.wutson.BuildConfig;
-import com.ataulm.wutson.R;
-import com.ataulm.wutson.jabber.Jabber;
-import com.squareup.okhttp.Credentials;
-import com.squareup.okhttp.MediaType;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.RequestBody;
-import com.squareup.okhttp.Response;
-
-import java.util.concurrent.TimeUnit;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import rx.Observable;
-import rx.functions.Func1;
 
 public abstract class SimpleAbstractAccountAuthenticator extends AbstractAccountAuthenticator {
 
