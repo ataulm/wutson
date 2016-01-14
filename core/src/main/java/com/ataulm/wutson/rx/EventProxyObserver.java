347aba2508d1a139fc6674692f2cc62594b163a6
Ataul Munim <hello@ataulm.com>
2016-01-14 20:07:19 +0000

    Add explicit developer error for errors in Event pipeline

--- core/src/main/java/com/ataulm/wutson/rx/EventProxyObserver.java
+++ core/src/main/java/com/ataulm/wutson/rx/EventProxyObserver.java
@@ -1,5 +1,6 @@
 package com.ataulm.wutson.rx;
 
+import com.ataulm.wutson.DeveloperError;
 import com.ataulm.wutson.Log;
 import com.ataulm.wutson.repository.event.Event;
 
@@ -18,6 +19,15 @@ public class EventProxyObserver<T> extends LoggingObserver<Event<T>> {
     public void onNext(Event<T> event) {
         super.onNext(event);
         observer.onNext(event);
+    }
+
+    @Override
+    public void onError(Throwable e) {
+        super.onError(e);
+        throw DeveloperError.because(
+                "Errors here indicate a problem in the pipeline. " +
+                        "The only errors we should get ought to be transformed into Events with type ERROR"
+        );
     }
 
 }
